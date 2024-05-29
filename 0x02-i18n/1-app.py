2e3a28000fc1766846df3a5a9af93e30b762e96f
SlimjimmyMax <slimjimmymax@outlook.com>
2024-05-29 18:50:44 +0300

    chore: Update Flask app configuration class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,15 +2,30 @@
 
 """
 Basic Flask app with Babel extension
+
+This module contains a Flask application that uses the Babel extension
+for internationalization and localization.
+
+The application has a single route '/' that renders the 'index.html' template.
+The locale for the template is determined by the 'get_locale' function,
+which selects the best match from the accepted languages in the request.
+
+The application configuration is defined in the 'Config' class, which sets
+the supported languages, default locale, and default timezone.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
     """
     Configuration class for the Flask app
+
+    Attributes:
+        LANGUAGES (list): A list of supported language codes.
+        BABEL_DEFAULT_LOCALE (str): The default locale for the app.
+        BABEL_DEFAULT_TIMEZONE (str): The default timezone for the app.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -24,6 +39,12 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """
+    Determines the locale for the current request.
+
+    Returns:
+        str: The best matching locale from the accepted languages in the request.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
@@ -31,6 +52,9 @@ def get_locale():
 def index():
     """
     Renders the index.html template
+
+    Returns:
+        str: The rendered index.html template.
     """
     return render_template('1-index.html')
 
