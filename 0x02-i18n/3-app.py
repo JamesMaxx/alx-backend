68a77e99f170cbaf390714c9745aeae5bd69a413
SlimjimmyMax <slimjimmymax@outlook.com>
2024-05-29 19:07:08 +0300

    chore: Refactor Flask app configuration class for improved readability and clarity

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -5,6 +5,7 @@ Basic Flask app with Babel extension
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
+
 class Config(object):
     """
     Configuration class for the Flask app
@@ -13,9 +14,11 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -24,12 +27,14 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """
     Renders the index.html template
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
