1fbe7b36c3eb06c47d9ccc98831ae3989342bc24
SlimjimmyMax <slimjimmymax@outlook.com>
2024-05-30 14:33:04 +0300

    Refactor render_template function to use correct index.html template

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -33,7 +33,7 @@ def index():
     """
     Renders the index.html template
     """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
